a78424c2fef51eb3deba434b2cde53daad60c44d
Irene Valle <irenevalle@MacBook-Pro-de-Mac.local>
2022-05-26 16:06:52 +0200

    logo

--- user-side/src/App.js
+++ user-side/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import bgImage from "./img/bg-image.png";
+import logo from "./img/logo2.png"
 import SearchBar from "./components/SearchBar";
+
 
 // import "./App.css";
 
@@ -39,10 +41,15 @@ function App() {
   return (
     <div className="h-screen relative w-screen">
       <img src={bgImage} className="-z-50 absolute h-full w-full object-cover"></img>
+      <div className="flex justify-between content-center mr-32">
       <SearchBar className="z-10"/>
-      <h1 className="text-3xl text-red-500 font-bold underline z-10 mx-20 mt-10">
+      <img src={logo} className="h-[210px]"></img>
+
+      </div>
+      <h1 className="text-3xl text-red-500 font-bold underline z-10 mx-20">
         Animals world!
       </h1>
+
 
       {/* <ul>
       {animals.map(a =>(
--- user-side/src/components/SearchBar.js
+++ user-side/src/components/SearchBar.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import lensIcon from "../img/magnifying-glass-icon.png";
 import element1 from "../img/element1.png";
+import logo from "../img/logo.png"
 
 function SearchBar(props) {
   return (
     <div>
       <div className="mx-20 pt-20 flex flex-col gap-2 ">
-      <image src={element1} className="w-10"></image>
-      <h1 className="ml-4 font-bold text-xl text-[#2F430D] z-10 bg-[#c57e13] w-40">Busca un animal:</h1>
-      <form className="relative">
-        <input className="rounded-lg w-72 absolute -z-10"></input>
-        <img src={lensIcon} className="z-10 w-[20px] pt-[2px] ml-[260px]"></img>
+      {/* <image src={element1} className="w-10"></image> ---->not working*/}
+      <h3 className="ml-4 font-bold text-xl text-[#2F430D] z-10 bg-[#c57e13] w-[166px]">Busca un animal:</h3>
+      <form className="flex items-center gap-10">
+        <div className="flex bg-white gap-2 rounded-lg">
+        <input className="rounded-lg w-72 h-7 px-3"></input>
+        <img src={lensIcon} className="z-10 w-18 h-6 pt-0.5 text-center"></img>
+        </div>
+
+        <button className="bg-[#9fb7be] px-4 rounded-lg text-white font-bold py-1 text-lg pb-0.5">Vés-hi!</button>
       </form>
 
       </div>
